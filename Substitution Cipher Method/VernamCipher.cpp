43ea0ac301f2e88f656715cf5c9d1afe9d275e80
01anuraganand <anuraganand7618@gmail.com>
2023-04-20 13:25:09 +0530

    Create VernamCipher.cpp

---
+++ Substitution Cipher Method/VernamCipher.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+char alphabet[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+//int alphabetNum[26] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 , 13, 14 ,15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
+string encryption(string plainText, string key)
+{
+	string encryptedText = "";
+	for(int i = 0; i<plainText.size(); i++)
+	{
+		int val = (plainText[i]-'A') + int(key[i]-'A');
+		if(val > 25)
+			encryptedText += alphabet[val-26];
+		else if(val >=0 && val <=25 )
+			encryptedText += alphabet[val];
+	}
+	return encryptedText;
+}
+
+string decryption(string encryptedText, string key)
+{
+	string decryptedText = "";
+	for(int i = 0; i<encryptedText.size(); i++)
+	{
+		int val = int(encryptedText[i]-'A') - int(key[i] - 'A');
+		if(val < 0)
+			decryptedText += alphabet[val+26];
+		else if(val >=0 && val <=25 )
+			decryptedText += alphabet[val];
+		
+	}
+	return decryptedText;
+}
+int main()
+{
+	string plainText, key;
+	cout<<"Enter the PlainText:"<<endl;
+	cin>>plainText;
+	cout<<"Enter the key (equal to size of PlainText):"<<endl;
+	cin>>key;
+	string EncryptedText = encryption(plainText, key);
+	cout<<"Encrypted Text:"<<EncryptedText<<endl;
+	string DecryptedText = decryption(EncryptedText, key);
+	cout<<"Decrypted Text:"<<DecryptedText<<endl;
+}
