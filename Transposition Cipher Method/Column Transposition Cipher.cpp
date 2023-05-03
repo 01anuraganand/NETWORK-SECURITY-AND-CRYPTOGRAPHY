c282445c3ec81aafc0d1853eb54171f2d423b968
01anuraganand <anuraganand7618@gmail.com>
2023-05-03 18:28:50 +0530

    Update Column Transposition Cipher.cpp

--- Transposition Cipher Method/Column Transposition Cipher.cpp
+++ Transposition Cipher Method/Column Transposition Cipher.cpp
@@ -1,59 +1,72 @@
 #include <iostream>
-#include <map>
+#include <vector>
 #include <string>
+#include <utility>
 using namespace std;
 
-void setPermutation(string &key, map<int, int> &alphabetOrder)
+pair<string, vector<vector<char>>> Encryption(vector<vector<char>> &pTmatrix)
 {
 	
-	for(int i = 0; i<key.size(); i++)
-		alphabetOrder[key[i]] = i;		
+	int row = pTmatrix.size(), col = pTmatrix[0].size();
+	vector<vector<char>> eTmatrix(col, vector<char>(row));
+	string encryptedText = "";
+	for(int i = 0; i<col; i++)
+		for(int j = 0; j<row; j++)
+			eTmatrix[i][j] = pTmatrix[j][i];
+		
+	for(auto vec1: eTmatrix)
+	{
+		for(auto it: vec1)
+			if(it != '\0')
+				encryptedText += it;
+	}
+	return {encryptedText, eTmatrix};
 }
 
-string Encryption(string plainText, string key)
+string Decryption(vector<vector<char>> &eTmatrix)
 {
-	map<int, int> alphabetOrder;
-	setPermutation(key, alphabetOrder);
+	int row = eTmatrix.size(), col = eTmatrix[0].size();
 	
-	int col = key.size();
-	int row = plainText.size()/col;
-	if(plainText.size() % col)
-		row +=1;
-	
-	string table[row][col];
-	int k = 0;
-	for(int i = 0; i<row; i++)
-	{
-		for(int j = 0; j<col; j++)
-		{
-			if(plainText[k] == '\0')
-				table[i][j] = '_',k++;
-			else
-				table[i][j] = plainText[k++];
-		}
-	}
-	
-	string encryptedText = "";
-	for(auto it: alphabetOrder)
-	{
-		int j = it.second; //store column number
-		for(int i = 0; i<row; i++)
-			encryptedText += table[i][j];
-	}
-	
-	for(int i = 0; i<row; i++)
-	{
-		for(int j = 0; j<col; j++)
-			cout<<table[i][j]<< " ";
-		cout<<endl;
-	}
-	return encryptedText;
+	vector<vector<char>> oTmatrix(col, vector<char>(row));
+	string decryptedText = "";
+	for(int i = 0; i<col; i++)
+		for(int j = 0; j<row; j++)
+			oTmatrix[i][j] = eTmatrix[j][i];
+		
+	for(auto vec1 : oTmatrix)
+		for(auto it : vec1)
+			if(it != '\0')
+				decryptedText += it;
+	return decryptedText;
 }
+
 int main()
 {
-	string plainText = "Geeks for Geeks";
-	string key = "HACK";
-	string encryptedText = Encryption(plainText, key);
-	cout<<"Encrypted Text:"<<encryptedText<<endl;
+	string plainText = "anurag anand";
+	int row, col = 5, k;
+	if(plainText.size() % col == 0)
+		row = plainText.size()/col;
+	else
+	{
+		row = (plainText.size()/col) + 1;
+		k = plainText.size()%col;
+	}
 	
+	vector<vector<char>> pTmatrix(row, vector<char>(col));
+	//Building columar table
+	for(int i = 0, count = 0; i<row; i++)
+	{
+		for(int j = 0; j<col; j++)
+			if(i == row-1 && j >=k)
+				pTmatrix[i][j] = '\0';
+			else
+				pTmatrix[i][j] = plainText[count++];
+	}
+	
+	
+	pair<string, vector<vector<char>>> encryptedText = Encryption(pTmatrix);
+	cout<<"Encrypted Text:"<<encryptedText.first<<endl;
+	
+	string decryptedText = Decryption(encryptedText.second);
+	cout<<"Decrypted Text:"<< decryptedText<<endl;
 }
